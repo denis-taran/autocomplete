0a1c486e238a38a2b57401a8a7e446f21a8c7c45
Denys Krasnoshchok <Denys Krasnoshchok>
2016-12-19 12:06:29 +0100

    typescript declaration file added

---
+++ autocomplete.d.ts
@@ -0,0 +1,19 @@
+export interface AutocompleteItem<T> {
+    label: string;
+    item: T;
+    group?: string;
+}
+export interface AutocompleteSettings<T> {
+    input: HTMLInputElement;
+    render?: (item: AutocompleteItem<T>) => HTMLDivElement | undefined;
+    renderGroup?: (name: string) => HTMLDivElement | undefined;
+    className?: string;
+    minLength?: number;
+    emptyMsg?: string;
+    onSelect: (item: T, input: HTMLInputElement) => void;
+    fetch: (text: string, update: (items: Array<AutocompleteItem<T>>) => void) => void;
+}
+export interface AutocompleteResult {
+    destroy: () => void;
+}
+export declare function autocomplete<T>(settings: AutocompleteSettings<T>): AutocompleteResult;
