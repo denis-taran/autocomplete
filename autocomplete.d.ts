3ea8e4398a15e6729ec69e0c4c64b2158c0ea1c6
Denys Krasnoshchok <kraaden@outlook.com>
2021-07-18 16:28:33 +0200

    added missing comments for settings

--- autocomplete.d.ts
+++ autocomplete.d.ts
@@ -7,18 +7,50 @@ export interface AutocompleteItem {
     group?: string;
 }
 export interface AutocompleteSettings<T extends AutocompleteItem> {
+    /**
+     * Autocomplete will be attached to this element.
+     */
     input: HTMLInputElement;
+    /**
+     * This method allows you to override the default rendering function for items.
+     * It must return a DIV element or undefined to skip rendering.
+     */
     render?: (item: T, currentValue: string) => HTMLDivElement | undefined;
+    /**
+     * This method allows you to override the default rendering function for item groups.
+     * It must return a DIV element or undefined to skip rendering.
+     */
     renderGroup?: (name: string, currentValue: string) => HTMLDivElement | undefined;
+    /**
+     * If specified, the autocomplete DOM element will have this class assigned to it.
+     */
     className?: string;
+    /**
+     * Specify the minimum text length required to show autocomplete.
+     */
     minLength?: number;
+    /**
+     * The message that will be showed when there are no suggestions that match the entered value.
+     */
     emptyMsg?: string;
+    /**
+     * This method will be called when user choose an item in autocomplete. The selected item will be passed as the first parameter.
+     */
     onSelect: (item: T, input: HTMLInputElement) => void;
     /**
      * Show autocomplete on focus event. Focus event will ignore the `minLength` property and will always call `fetch`.
      */
     showOnFocus?: boolean;
+    /**
+     * This method will be called to prepare suggestions and then pass them to autocomplete.
+     * @param {string} text - text in the input field
+     * @param {(items: T[] | false) => void} update - a callback function that must be called after suggestions are prepared
+     */
     fetch: (text: string, update: (items: T[] | false) => void, trigger: EventTrigger) => void;
+    /**
+     * Enforces that the fetch function will only be called once within the specified time frame (in milliseconds) and
+     * delays execution. This prevents flooding your server with AJAX requests.
+     */
     debounceWaitMs?: number;
     /**
      * Callback for additional autocomplete customization
