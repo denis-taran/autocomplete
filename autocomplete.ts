919fe9507c3b48601905d936b796be9be1859178
Denys Krasnoshchok <kraaden@outlook.com>
2021-08-21 00:36:25 +0200

    use a shorter syntax for string lookup

--- autocomplete.ts
+++ autocomplete.ts
@@ -19,52 +19,63 @@ export interface AutocompleteSettings<T extends AutocompleteItem> {
      * Autocomplete will be attached to this element.
      */
     input: HTMLInputElement;
+
     /**
      * Provide your own container for the widget.
      * If not specified, a new DIV element will be created.
      */
     container?: HTMLDivElement;
+
     /**
      * This method allows you to override the default rendering function for items.
      * It must return a DIV element or undefined to skip rendering.
      */
     render?: (item: T, currentValue: string) => HTMLDivElement | undefined;
+
     /**
      * This method allows you to override the default rendering function for item groups.
      * It must return a DIV element or undefined to skip rendering.
      */
     renderGroup?: (name: string, currentValue: string) => HTMLDivElement | undefined;
+
     /**
      * If specified, the autocomplete DOM element will have this class assigned to it.
      */
     className?: string;
+
     /**
      * Specify the minimum text length required to show autocomplete.
      */
     minLength?: number;
+
     /**
      * The message that will be showed when there are no suggestions that match the entered value.
      */
     emptyMsg?: string;
+
     /**
      * This method will be called when user choose an item in autocomplete. The selected item will be passed as the first parameter.
      */
     onSelect: (item: T, input: HTMLInputElement) => void;
+
     /**
      * Show autocomplete on focus event. Focus event will ignore the `minLength` property and will always call `fetch`.
      */
     showOnFocus?: boolean;
+
     /**
      * This method will be called to prepare suggestions and then pass them to autocomplete.
      * @param {string} text - text in the input field
      * @param {(items: T[] | false) => void} update - a callback function that must be called after suggestions are prepared
      */
     fetch: (text: string, update: (items: T[] | false) => void, trigger: EventTrigger) => void;
+
     /**
      * Enforces that the fetch function will only be called once within the specified time frame (in milliseconds) and
      * delays execution. This prevents flooding your server with AJAX requests.
      */
     debounceWaitMs?: number;
+
     /**
      * Callback for additional autocomplete customization
      * @param {HTMLInputElement} input - input box associated with autocomplete
@@ -73,10 +84,12 @@ export interface AutocompleteSettings<T extends AutocompleteItem> {
      * @param {number} maxHeight - max height that can be used by autocomplete
      */
     customize?: (input: HTMLInputElement, inputRect: ClientRect | DOMRect, container: HTMLDivElement, maxHeight: number) => void;
+
     /**
      * Prevents automatic form submit when ENTER is pressed
      */
     preventSubmit?: boolean;
+    
     /**
      * Prevents the first item in the list from being selected automatically. This option allows you
      * to submit a custom text by pressing ENTER even when autocomplete is displayed.
@@ -113,7 +126,7 @@ export default function autocomplete<T extends AutocompleteItem>(settings: Autoc
     const container: HTMLDivElement = settings.container || doc.createElement("div");
     const containerStyle = container.style;
     const userAgent = navigator.userAgent;
-    const mobileFirefox = userAgent.indexOf("Firefox") !== -1 && userAgent.indexOf("Mobile") !== -1;
+    const mobileFirefox = ~userAgent.indexOf("Firefox") && ~userAgent.indexOf("Mobile");
     const debounceWaitMs = settings.debounceWaitMs || 0;
     const preventSubmit = settings.preventSubmit || false;
     const disableAutoSelect = settings.disableAutoSelect || false;
